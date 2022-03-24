8dbe4409c8f7896c51aabe835ff22c38a8f22ce8
Enrico La Torre <enrico.latorre@aldi-sued.com>
2022-03-24 16:55:25 +0100

    channels, routines, apiGET and POST

--- day04/goRoutines04.go
+++ day04/goRoutines04.go
@@ -6,10 +6,11 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var (
-	mutex1   sync.Mutex
+	mutex10  sync.Mutex
 	balance1 int
 )
 
@@ -18,34 +19,46 @@ func init() {
 }
 
 func deposit1(value int, wg *sync.WaitGroup) {
-	wg.Add(1)
+	//wg.Add(1)
 	go depositImpl(value)
 	wg.Done()
 }
 
 func depositImpl(value int) {
 	fmt.Printf("Depositing %d to account with balance1: %d\n", value, balance1)
+	mutex10.Lock()
 	balance1 += value
+	mutex10.Unlock()
 }
 
 func withdraw1(value int, wg *sync.WaitGroup) {
-	wg.Add(1)
+	//wg.Add(1)
 	go withdrawImpl(value)
+	//time.Sleep(time.Second * 3)
 	wg.Done()
 }
 
 func withdrawImpl(value int) {
 	fmt.Printf("Withdrawing %d from account with balance1: %d\n", value, balance1)
+	mutex10.Lock()
 	balance1 -= value
+	mutex10.Unlock()
 }
 
 func main() {
 	var wg1 sync.WaitGroup
-
+	wg1.Add(3)
 	withdraw1(750, &wg1) // if you pass a WaitGroup to a function, use a pointer
 	deposit1(1000, &wg1)
 	withdraw1(500, &wg1)
+
+	time.Sleep(time.Second)
 	wg1.Wait() // wait until we finish
 
 	fmt.Printf("New Balance %d\n", balance1)
+
+	// This doesnt work because the methods are called so fast that the "go" never is invoked and
+	// main is finished
+	// Wait just wants to see 3 items in wait group, which exist at wg1.Wait()
+	// Only works with sleeps
 }
